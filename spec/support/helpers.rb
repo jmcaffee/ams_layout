e37131db4ef6f3b4a53912ecff51f529af3bf69b
Jeff McAffee <jeff@ktechsystems.com>
2014-07-08 08:40:50 -0700

    Add mock_client method

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -11,6 +11,20 @@ def client
   $client ||= AmsLayout::Client.new
 end
 
+def mock_client(field_data = {})
+  mock_client = object_double(AmsLayout::Client.new)
+
+  allow(mock_client).to receive(:login).with(anything, anything).and_return(nil)
+  allow(mock_client).to receive(:logout).and_return(nil)
+  allow(mock_client).to receive(:quit).and_return(nil)
+  allow(mock_client).to receive(:get_field_data).and_return(field_data)
+  allow(mock_client).to receive(:write_layout).with(anything, anything).and_return(true)
+  allow(mock_client).to receive(:write_layout_class).with(anything, anything).and_return(true)
+  allow(mock_client).to receive(:write_delegate_class).with(anything, anything).and_return(true)
+
+  mock_client
+end
+
 def load_yml filename
   YAML::load_file(filename)
 end
